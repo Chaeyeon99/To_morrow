1fcc63c223730c02d46d87c85a1bd77f12e551c8
HyeM207 <63229014+HyeM207@users.noreply.github.com>
2021-08-23 23:57:42 +0900

    Refactor login

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
-from django.contrib import auth
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import make_password, check_password
+
 
 from .forms import UserCreationForm
 from .models import Member
@@ -12,31 +13,12 @@ from .models import Member
 def Index (request):
     return render(request,'accounts/Index.html',None) 
 
-# 일반 유저 회원가입
-# def signup(request): 
-#     if request.method == "POST":
-#         form = addUserForm(request.POST)
-#         if form.is_valid():
-#             form.save() 
-#             memberId = form.cleaned_data.get('memberId')  
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(memberId=memberId, password=raw_password) 
-#             #login(request, user) 
-#             return HttpResponseRedirect('/accounts')
-#     else:
-#         form = addUserForm()
-#     return render(request, 'accounts/signup.html', {'form': form})
-
-
+# 회원가입
 def signup(request): 
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save() 
-            memberId = form.cleaned_data.get('memberId')  
-            raw_password = form.cleaned_data.get('password1')
-            # user = authenticate(memberId=memberId, password=raw_password) 
-            #login(request, user) 
+            form.save()             
             return HttpResponseRedirect('/accounts')
     else:
         form = UserCreationForm()
@@ -44,53 +26,29 @@ def signup(request):
 
 
 #로그인
-# def login(request): 
-#     if request.user.is_authenticated:
-#         return HttpResponseRedirect('/account/Index')
+def login(request): 
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/accounts/Index')
 
-#     if request.method == 'POST':
-#         form = AuthenticationForm(request=request, data=request.POST)
-#         if form.is_valid():
-#             memberId = form.cleaned_data.get('memberId')
-#             raw_password = form.cleaned_data.get('password1')
-#             member = authenticate(memberId=memberId, password=raw_password)
-#             if member is not None:
-#                 print(member)
-#                 login(request, member) 
-#                 return HttpResponseRedirect('/accounts')
-#             else:
-#                 print('User not found')
-#         else:
-#             return render(request, 'accounts/login.html', {'form': form})
-    
-#     if request.method == 'GET':
-#         form = AuthenticationForm()
-    
-#     return render(request, 'accounts/login.html', {'form': form})
-    
-# def login(request):
-#     username=request.POST['username']
-#     password=request.POST['password']
-#     Member.user=authenticate(request,username=username,password=password)
-#     if Member.user is not None:
-#         login(request, Member.user)
-#         return HttpResponseRedirect('/accounts')
-#     else:
-#         return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
 
-#     if request.user.is_authenticated:
-#         return HttpResponseRedirect('/account/Index')
-#     if request.method=='POST':
-#         user=authenticate(username=username, password=password)
-#     if user.is_valid():
-#         if user is not None:
-#             if user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect('/accounts')
-#             else:
-#                 print('User not found')
-#         else:
-#             return render(request, 'accounts/login.html', {'user':user})
-#     if request.method =='GET':
-#         user=AuthenticationForm()
-#     return render(request, 'accounts/login.html', {'user': user})
+        if form.is_valid():
+            print("cleaned_data : ", form.cleaned_data)
+            memberId = form.cleaned_data['username']
+            print("memberId : ", memberId)
+            raw_password = form.cleaned_data['password']
+            member = Member.objects.get(memberId=memberId)
+            
+            if check_password(raw_password, member.password):
+                request.session['user']=member.memberId
+                return redirect('/accounts')
+            else:
+                print('비밀번호를 틀렸습니다.')
+    
+    if request.method == 'GET':
+        form = AuthenticationForm()
+    
+    return render(request, 'accounts/login.html', {'form': form})
+
+    
